7fcf469978d1814958193d72cf77d93fd286b60e
rafpang <rafibintangpagi@gmail.com>
2023-02-09 13:50:15 +0800

    fixed indent

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const ELEMENT_VISIBLE_TRIGGER = 100;
 
 /* ------- css animations on content upon scroll up ------ */
-const reveal = function() {
-    var reveals = document.querySelectorAll(".reveal");
-    reveals.forEach(item => {
-        var windowHeight = window.innerHeight;
-        var elementTop = item.getBoundingClientRect().top;
-        var elementVisible = ELEMENT_VISIBLE_TRIGGER;
+const reveal = function () {
+  var reveals = document.querySelectorAll(".reveal");
+  reveals.forEach((item) => {
+    var windowHeight = window.innerHeight;
+    var elementTop = item.getBoundingClientRect().top;
+    var elementVisible = ELEMENT_VISIBLE_TRIGGER;
 
-        if (elementTop < windowHeight - elementVisible) {
-            item.classList.add("current");
-        } else {
-            item.classList.remove("current");
-        }
-    })
-}
+    if (elementTop < windowHeight - elementVisible) {
+      item.classList.add("current");
+    } else {
+      item.classList.remove("current");
+    }
+  });
+};
 window.addEventListener("scroll", reveal);
-
